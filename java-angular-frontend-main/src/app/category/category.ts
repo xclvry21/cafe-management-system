2bc624ce169366370a2565d372a452302bda1571
xcl21 <axcle.viray21@gmail.com>
2023-07-03 21:18:30 +0800

    frontend commit

---
+++ java-angular-frontend-main/src/app/category/category.ts
@@ -0,0 +1,7 @@
+export interface Category {
+    id: number;
+    name: string;
+    type: string;
+    size: string;
+    price: number;
+}
---
+++ java-angular-frontend-main/src/app/category/index/index.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../category.service';
+import { Category } from '../category';
+
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent implements OnInit {
+
+  categories: Category[] = [];
+  /*------------------------------------------
+    --------------------------------------------
+    Created constructor
+    --------------------------------------------
+    --------------------------------------------*/
+  constructor(public categoryService: CategoryService) { }
+    
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+  ngOnInit(): void {
+    this.categoryService.getAll().subscribe((data: Category[])=>{
+      this.categories = data;
+      console.log(this.categories);
+    })  
+  }
+
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+   deleteCategory(id:number){
+    if(confirm("Are you sure to delete this record?"))
+    this.categoryService.delete(id).subscribe(res => {
+         this.categories = this.categories.filter(item => item.id !== id);
+         alert("Record deleted successfully")
+         console.log('Product deleted successfully!');
+    })
+  }
+
+}
